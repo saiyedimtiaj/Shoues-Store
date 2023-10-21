8dc28f3635bef88ecd8849c4a136b7df8845548f
saiyedimtiaj <hridoyemtiaz0@gmail.com>
2023-10-21 16:17:48 +0600

    readmi file and live link

--- src/Pages/Update/Update.jsx
+++ src/Pages/Update/Update.jsx
@@ -21,7 +21,7 @@ const Update = () => {
         const rating = form.rating.value;
         const update = {name,image,brand,type,price,description,rating}
 
-        fetch(`https://assingment-10-server-eta.vercel.app/${loader.brand}/${loader._id}`,{
+        fetch(`https://assingment-10-server-eta.vercel.app/products/${loader.brand}/${loader._id}`,{
             method:'PUT',
             headers:{
                 'content-type':'application/json'
